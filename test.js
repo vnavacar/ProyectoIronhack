9169b397c7c684574102fa139c59e3e278143fb8
vnavacar <98817213+vnavacar@users.noreply.github.com>
2024-03-21 16:27:07 +0100

    ej 2

--- test.js
+++ test.js
@@ -1,5 +1,21 @@
-let sum = 0;
-for (let i = 1; i <= 100; i++) {
-  sum += i;
+
+function sum100() {
+    let sum = 0;
+    for (let i = 1; i <= 100; i++) {
+      sum += i;
+    }
+    console.log(sum);
 }
-console.log(sum);
+
+function colorMix(color1, color2) {
+    if (color1 === 'red' && color2 === 'yellow') {
+      return 'orange';
+    } else if (color1 === 'red' && color2 === 'blue') {
+        return 'purple';
+        }
+    else if (color1 === 'yellow' && color2 === 'blue') {
+        return 'green';
+    } else {
+        return 'invalid';
+    }
+}
